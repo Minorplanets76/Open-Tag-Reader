e8682c98ec51de2cfc575172c943cb9cbb4f12a8
Chris Chad <107326485+Minorplanets76@users.noreply.github.com>
2024-04-27 09:56:03 +1000

    .

--- t-embed/OTR_T-embed/src/credentials.h
+++ t-embed/OTR_T-embed/src/credentials.h
@@ -3,7 +3,7 @@
 #define CREDENTIALS_H
 // Update these credentials
 
-#define WIFI_SSID             "DADSFFICEPC"
-#define WIFI_PASSWORD         "Demelza73"
+#define WIFI_SSID             "ssid"
+#define WIFI_PASSWORD         "password3"
 
 #endif
