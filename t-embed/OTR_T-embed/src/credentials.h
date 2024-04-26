c76b2c8a8f27e232cebeb7d6fe280192c02c7463
Minorplanets76 <chris.chad76@gmail.com>
2024-04-26 18:13:29 +1000

    update

--- t-embed/OTR_T-embed/src/credentials.h
+++ t-embed/OTR_T-embed/src/credentials.h
@@ -1,7 +1,7 @@
 // credentials.h
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
-
+// Update these credentials
 
 #define WIFI_SSID             "Your-SSID"
 #define WIFI_PASSWORD         "Your-password"
